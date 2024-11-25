03260feef900f1da9f271eb12fac58a7c826cee3
Fiston <karekezifiston30@gmail.com>
2024-11-25 19:19:33 +0200

    a

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
     
     const [cartItems, setCartItems] = useState({});
 
-    const url ="http://localhost:4000"
+    const url ="http://localhost:5000"
     const [token,setToken]=useState("")
     const [food_list,setFoodList] =useState([])
 
