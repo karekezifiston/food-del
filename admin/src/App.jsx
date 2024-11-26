5f9a311072120382cf3c958fb20ec4485662eb6d
Fiston <karekezifiston30@gmail.com>
2024-11-26 11:19:45 +0200

    a

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "https://food-eco-backend.onrender.com"
+  const url ="http://localhost:4000";
 
   return (
     <div className='app'>
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
     
     const [cartItems, setCartItems] = useState({});
 
-    const url ="https://food-eco-backend.onrender.com"
+    const url ="http://localhost:4000";
     const [token,setToken]=useState("")
     const [food_list,setFoodList] =useState([])
 
