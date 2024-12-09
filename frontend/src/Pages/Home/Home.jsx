8fa519bd2f380f43f9e8eef530ad9dd8b70ab041
Fiston <karekezifiston30@gmail.com>
2024-12-09 17:31:59 +0200

    a

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
