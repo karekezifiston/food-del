1475aa5c59ced16b6ca29332bd23b2a5d61d67bd
KAREKEZI <karekezifiston30@gmail.com>
2025-06-26 18:40:18 +0200

    Update Home.jsx

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react'
-import Header from '../../Components/Header/Header'
-import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../Components/AppDownload/AppDownload'
-const Home = () => {
+import React, { useState, useRef } from 'react';
+import Header from '../../Components/Header/Header';
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../Components/AppDownload/AppDownload';
 
-  const [category,setCategory]=useState("All");
+const Home = () => {
+  const [category, setCategory] = useState("All");
+
+  // Create a ref to scroll to the menu
+  const menuRef = useRef(null);
+
+  // Function to scroll
+  const scrollToMenu = () => {
+    menuRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
-      <Header/>
-      <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category}/>
-      <AppDownload/>
-    </div>
-  )
-}
+      <Header scrollToMenu={scrollToMenu} />
+      
+      {/* Wrap ExploreMenu in a div with the ref */}
+      <div ref={menuRef}>
+        <ExploreMenu category={category} setCategory={setCategory} />
+      </div>
 
-export default Home
+      <FoodDisplay category={category} />
+      <AppDownload />
+    </div>
+  );
+};
+
+export default Home;
