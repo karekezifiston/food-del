fd8ab7eb847e235ce78f572289a696b65f870ac0
KAREKEZI <karekezifiston30@gmail.com>
2025-06-26 18:40:54 +0200

    Update Header.jsx

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Header.css'
-const Header = () => {
+import React from 'react';
+import './Header.css';
+
+const Header = ({ scrollToMenu }) => {
   return (
     <div className='header'>
-        <div className="header-contents">
-          <h2>Order now, enjoy your favorite meal.</h2>
-          <p>Choose your a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your  dining experience, one delecious meal at a time.</p>
-          <button>View Menu</button>
-          <h2></h2>
-        </div>
+      <div className="header-contents">
+        <h2>Order now, enjoy your favorite meal.</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.
+        </p>
+        <button onClick={scrollToMenu}>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
